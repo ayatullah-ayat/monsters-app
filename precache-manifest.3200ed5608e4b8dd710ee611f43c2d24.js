8a771bb158346618fc69c657e38308993363698d
Ayatullah khamini <ayat2486@gmail.com>
2019-11-08 21:24:31 +0600

    Updates

---
+++ precache-manifest.3200ed5608e4b8dd710ee611f43c2d24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5daebfefb02939af17af69ba618eda4",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "4ecfd1a601586402c14c",
+    "url": "/monsters-app/static/css/main.d29929db.chunk.css"
+  },
+  {
+    "revision": "4dd9315dacd6d0eec06a",
+    "url": "/monsters-app/static/js/2.89b49aee.chunk.js"
+  },
+  {
+    "revision": "4ecfd1a601586402c14c",
+    "url": "/monsters-app/static/js/main.0ea4e8b4.chunk.js"
+  },
+  {
+    "revision": "893ccad28627e976a7c4",
+    "url": "/monsters-app/static/js/runtime-main.71fc94f5.js"
+  }
+]);
